053a27168fa5ecdd36a6a86e61b5a0ac05392875
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-03-25 18:19:29 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
-docker build -p 8888:8888 --file Dockerfile -t $(basename $PWD) .
-nvidia-docker run --tty --interactive  $(basename $PWD)
+docker build --file Dockerfile -t $(basename $PWD) .
+nvidia-docker run --tty --interactive --publish 8888:8888 $(basename $PWD)
